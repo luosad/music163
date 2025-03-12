49bd7ec225f6f2601a8954f3246dd867ecf42241
luo <2856591213@qq.com>
2025-03-12 16:24:55 +0800

    修改了ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,34 @@ import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended
-]
+/** @type {import('eslint').Linter.Config} */
+export default {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  globals: globals.browser,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
+  ],
+  rules: {
+    // 自定义规则
+  },
+  settings: {
+    react: {
+      version: 'detect' // 自动检测 React 版本
+    }
+  }
+}
